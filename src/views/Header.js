adac35888189617a5e70c3059e1a2bfdaddb0e28
gretajonsson <gretaj@kth.se>
2022-11-30 14:39:07 +0100

    Header update

--- src/views/Header.js
+++ src/views/Header.js
@@ -12,40 +12,39 @@ import "../components/css/Headings.css";
 
 export default function Header(props) {
   return (
-    <div className="Options">
-      <div>
-        <BiSearch />
-        <a className="link" herf="#">
-          {" "}
-          Link 1
-        </a>
-      </div>
-      <div>
-        <BiDrink />
-        <a className="link" href="#">
-          {" "}
-          Link 1
-        </a>
-      </div>
-      <div>
-        <BiBook />
-        <a className="link" href="#">
-          {" "}
-          Link 1
-        </a>
-      </div>
-      <div>
-        <BiHeart />
-        <a className="link" href="#">
-          Link 1
-        </a>
-      </div>
-      <div>
-        <BiShapePolygon />
-        <a className="link" href="#">
-          {" "}
-          Link 1
-        </a>
+    <div className="Header">
+      <div>Logo</div>
+      <div className="Options">
+        <div>
+          <BiSearch />
+          <a className="link" herf="#">
+            Link 1
+          </a>
+        </div>
+        <div>
+          <BiDrink />
+          <a className="link" href="#">
+            Link 1
+          </a>
+        </div>
+        <div>
+          <BiBook />
+          <a className="link" href="#">
+            Link 1
+          </a>
+        </div>
+        <div>
+          <BiHeart />
+          <a className="link" href="#">
+            Link 1
+          </a>
+        </div>
+        <div>
+          <BiShapePolygon />
+          <a className="link" href="#">
+            Link 1
+          </a>
+        </div>
       </div>
     </div>
   );
