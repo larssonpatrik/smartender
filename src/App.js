a38854c6c873266aad926cda2ab0a7af29d20790
Patrik Larsson <patrik@dictodesigns.com>
2022-11-30 10:47:39 +0100

    fixed api calls

--- src/App.js
+++ src/App.js
@@ -10,23 +10,32 @@ import {
 } from "./DrinkSource";
 import resolvePromise from "./resolvePromise";
 import promiseNoData from "./promiseNoData";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import PaddanDevEnv from "./PaddDevEnv";
 
 function App() {
-  const [promiseState, setPromiseState] = React.useState({});
-  const [, rerender] = React.useState();
+  const [promiseState] = React.useState({});
+  const [, reRender] = React.useState();
+
+  function notifyACB() {
+    reRender({});
+  }
 
   //listIngredients();
   //searchDrinkByName("vodka");
   //filterDrinkByIngridient("Gin, Lime");
   //filterDrinkByCategory("classic");
   //filterIngredientByAlcoholic("non-alcoholic");
-  function testACB() {
-    resolvePromise(searchDrinkByName("vodka"), promiseState);
-    console.log(promiseState);
-  }
-  return <div>Hello World</div>;
+
+  React.useEffect(() => {
+    resolvePromise(searchDrinkByName("gin"), promiseState, notifyACB);
+  }, []);
+
+  return (
+    <div>
+      {promiseNoData(promiseState) || (
+        <p>{promiseState.data.drinks[1].strDrink}</p>
+      )}
+    </div>
+  );
 }
 
 export default App;
