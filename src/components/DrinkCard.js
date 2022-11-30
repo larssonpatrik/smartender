a6eadc0f44c0226c373cd336bbc64ea08b9d71d8
Samuel Ribaric <ribaric@kth.se>
2022-11-30 12:10:11 +0100

    Update DrinkCard.js

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -1,9 +1,4 @@
 import "./css/DrinkCard.css";
-
 export function DrinkCard(props) {
-  return (
-    //Change img to img tag with drink prop "img"
-
-    <img className="img" src={props.img} alt="drink" />
-  );
+  return <img className="img" src={props.img} alt="drink" />;
 }
