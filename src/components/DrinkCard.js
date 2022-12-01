3cc42309d07ed7b4a11c000cb07b947b64400a83
Samuel Ribaric <ribaric@kth.se>
2022-12-01 13:43:06 +0100

    Testing some UI Stuff

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -1,9 +1,13 @@
 import { HeadingThree } from "./Headings";
 import "./css/DrinkCard.css";
 export function DrinkCard(props) {
+  function onClickACB() {
+    console.log("clicky");
+  }
   return (
     <div className="container">
       <img className="img" src={props.img} alt="drink" />
+      <button onClick={onClickACB}>Check out the drink!</button>
       <HeadingThree>{props.name}</HeadingThree>
     </div>
   );
