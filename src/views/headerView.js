5610608e55fe96657dd0281f787930ac6c69864e
Samuel Ribaric <ribaric@kth.se>
2022-12-02 18:05:10 +0100

    Update headerView.js

--- src/views/headerView.js
+++ src/views/headerView.js
@@ -16,39 +16,38 @@ import { PrimaryButton, SecondaryButton } from "../components/Buttons";
 
 export default function Header(props) {
   return (
-    <div style={{display: 'flex', justifyContent: 'center'}}>
-
-    <div className="Header">
-      <p style={{color:'whitesmoke'}}>Logga</p>
-      <div className="Options">
-        <div>
-          <a className="link" href="#">
-            <BiHome />
-            Home
-          </a>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="Header">
+        <p style={{ color: "whitesmoke" }}>Logga</p>
+        <div className="Options">
+          <div>
+            <a className="link" href="#">
+              <BiHome />
+              Home
+            </a>
+          </div>
+          <div>
+            <a className="link" href="#">
+              <BiSearch /> Advanced search
+            </a>
+          </div>
+          <div>
+            <a className="link" href="#">
+              <BiDrink /> Randomize
+            </a>
+          </div>
+          <div>
+            <a className="link" href="#">
+              <BiHeart /> Favourites
+            </a>
+          </div>
         </div>
-        <div>
-          <a className="link" href="#">
-            <BiSearch /> Advanced search
-          </a>
-        </div>
-        <div>
-          <a className="link" href="#">
-            <BiDrink /> Randomize
-          </a>
-        </div>
-        <div>
-          <a className="link" href="#">
-            <BiHeart /> Favourites
-          </a>
-        </div>
-      </div>
-      <div className="btns">
+        <div className="btns">
           <SecondaryButton>Log in</SecondaryButton>
-          <Spacer size={2}/>
+          <Spacer size={2} />
           <PrimaryButton>Sign up</PrimaryButton>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
