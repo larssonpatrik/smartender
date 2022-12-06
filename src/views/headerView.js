a9d022550c2ca3e830d9ff4dd193dd13270472b1
Samuel Ribaric <ribaric@kth.se>
2022-12-06 13:15:32 +0100

    added logotype to header

--- src/views/headerView.js
+++ src/views/headerView.js
@@ -19,26 +19,26 @@ export default function Header(props) {
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div className="Header">
-        <p style={{ color: "whitesmoke" }}>Logga</p>
+        <img className="Img" src="https://iili.io/HC6Ypqb.png"></img>
         <div className="Options">
           <div>
-            <Link className='link' to="/">
+            <Link className="link" to="/">
               <BiHome />
               Home
             </Link>
           </div>
           <div>
-            <Link className='link' to="/search">
+            <Link className="link" to="/search">
               <BiSearch /> Advanced search
             </Link>
           </div>
           <div>
-            <Link className='link' to="/randomize">
+            <Link className="link" to="/randomize">
               <BiDrink /> Randomize
             </Link>
           </div>
           <div>
-            <Link className='link' to="/favorites">
+            <Link className="link" to="/favorites">
               <BiHeart /> Favourites
             </Link>
           </div>
