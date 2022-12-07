67689707e274561646576cd3137d4c1b71d46976
larssonpatrik <Patrik@dictodesigns.com>
2022-12-07 12:24:48 +0100

    more efficient code DrinkSource

--- src/DrinkSource.js
+++ src/DrinkSource.js
@@ -1,7 +1,18 @@
 import { API_KEY } from "./APIConfig";
 
-// SEARCH FUNCTIONS
-export function searchAPICall(searchQuery) {
+// AVAILABLE ENDPOINTS
+const endpoints = {
+  search: "search.php",
+  filter: "filter.php",
+  list: "list.php",
+  lookup: "lookup.php",
+  random: "random.php",
+  popular: "popular.php",
+  latest: "latest.php",
+};
+
+// GENERAL CALL FOR ALL CALLS
+export function APICall(endpoint, searchQuery) {
   function throwError() {
     throw new Error("Something went nuts! Try again!");
   }
@@ -29,7 +40,7 @@ export function searchAPICall(searchQuery) {
     }
 
     return fetch(
-      "https://the-cocktail-db.p.rapidapi.com/search.php" + queryString,
+      "https://the-cocktail-db.p.rapidapi.com/" + endpoint + queryString,
       options
     )
       .then(treatHTTPResponseACB)
@@ -38,255 +49,61 @@ export function searchAPICall(searchQuery) {
   });
 }
 
+// SEARCH FUNCTIONS
 export function searchDrinkByName(searchQuery) {
-  return searchAPICall({ s: searchQuery });
+  return APICall(endpoints.search, { s: searchQuery });
 }
 
 export function searchIngredientByName(searchQuery) {
-  return searchAPICall({ i: searchQuery });
+  return APICall(endpoints.search, { i: searchQuery });
 }
 
 // FILTER FUNCTIONS
-export function filterAPICall(searchQuery) {
-  function throwError() {
-    throw new Error("Something went nuts! Try again!");
-  }
-  function treatHTTPResponseACB(resp) {
-    return resp.status === 200 ? resp.json() : throwError();
-  }
-
-  let queryString = "?" + new URLSearchParams(searchQuery).toString();
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
-    },
-  };
-
-  return new Promise(async function createPromiseACB(resolve, reject) {
-    function resolvePromiseACB(res) {
-      resolve(res);
-    }
-
-    function rejectPromiseACB(err) {
-      reject(err);
-    }
-
-    return fetch(
-      "https://the-cocktail-db.p.rapidapi.com/filter.php" + queryString,
-      options
-    )
-      .then(treatHTTPResponseACB)
-      .then(resolvePromiseACB)
-      .catch(rejectPromiseACB);
-  });
-}
-
 export function filterDrinkByIngridient(searchQuery) {
-  return filterAPICall({ i: searchQuery });
+  return APICall(endpoints.filter, { i: searchQuery });
 }
 
 export function filterDrinkByCategory(searchQuery) {
-  return filterAPICall({ c: searchQuery });
+  return APICall(endpoints.filter, { c: searchQuery });
 }
 
 export function filterIngredientByAlcoholic(searchQuery) {
-  return filterAPICall({ a: searchQuery });
+  return APICall(endpoints.filter, { a: searchQuery });
 }
 
 // LIST FUNCTIONS
-export function listAPICall(searchQuery) {
-  function throwError() {
-    throw new Error("Something went nuts! Try again!");
-  }
-  function treatHTTPResponseACB(resp) {
-    return resp.status === 200 ? resp.json() : throwError();
-  }
-
-  let queryString = "?" + new URLSearchParams(searchQuery).toString();
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
-    },
-  };
-
-  return new Promise(async function createPromiseACB(resolve, reject) {
-    function resolvePromiseACB(res) {
-      resolve(res);
-    }
-
-    function rejectPromiseACB(err) {
-      reject(err);
-    }
-
-    return fetch(
-      "https://the-cocktail-db.p.rapidapi.com/list.php" + queryString,
-      options
-    )
-      .then(treatHTTPResponseACB)
-      .then(resolvePromiseACB)
-      .catch(rejectPromiseACB);
-  });
-}
-
 export function listIngredients() {
-  return listAPICall({ i: "list" });
+  return APICall(endpoints.list, { i: "list" });
 }
 
 export function listCategories() {
-  return listAPICall({ c: "list" });
+  return APICall(endpoints.list, { c: "list" });
 }
 
 export function listGlasses() {
-  return listAPICall({ g: "list" });
+  return APICall(endpoints.list, { g: "list" });
 }
 
 export function listAlcoholics() {
-  return listAPICall({ a: "list" });
+  return APICall(endpoints.list, { a: "list" });
 }
 
-// LOOKUP FUNCTIONS
-export function lookUpAPICall(searchQuery) {
-  function throwError() {
-    throw new Error("Something went nuts! Try again!");
-  }
-  function treatHTTPResponseACB(resp) {
-    return resp.status === 200 ? resp.json() : throwError();
-  }
-
-  let queryString = "?" + new URLSearchParams(searchQuery).toString();
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
-    },
-  };
-
-  return new Promise(async function createPromiseACB(resolve, reject) {
-    function resolvePromiseACB(res) {
-      resolve(res);
-    }
-
-    function rejectPromiseACB(err) {
-      reject(err);
-    }
-
-    return fetch(
-      "https://the-cocktail-db.p.rapidapi.com/lookup.php" + queryString,
-      options
-    )
-      .then(treatHTTPResponseACB)
-      .then(resolvePromiseACB)
-      .catch(rejectPromiseACB);
-  });
-}
-
+// GET FUNCTIONS
 export function getDrinkById(id) {
-  return lookUpAPICall({ i: id });
+  return APICall(endpoints.lookup, { i: id });
 }
 
-// RANDOM DRINK API CALL
+// RANDOM DRINK
 export function getRandomDrink() {
-  function throwError() {
-    throw new Error("Something went nuts! Try again!");
-  }
-  function treatHTTPResponseACB(resp) {
-    return resp.status === 200 ? resp.json() : throwError();
-  }
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
-    },
-  };
-
-  return new Promise(async function createPromiseACB(resolve, reject) {
-    function resolvePromiseACB(res) {
-      resolve(res);
-    }
-
-    function rejectPromiseACB(err) {
-      reject(err);
-    }
-
-    return fetch("https://the-cocktail-db.p.rapidapi.com/random.php", options)
-      .then(treatHTTPResponseACB)
-      .then(resolvePromiseACB)
-      .catch(rejectPromiseACB);
-  });
+  return APICall(endpoints.random);
 }
 
 // MOST POPULAR
 export function getPopularDrinks() {
-  function throwError() {
-    throw new Error("Something went nuts! Try again!");
-  }
-  function treatHTTPResponseACB(resp) {
-    return resp.status === 200 ? resp.json() : throwError();
-  }
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
-    },
-  };
-
-  return new Promise(async function createPromiseACB(resolve, reject) {
-    function resolvePromiseACB(res) {
-      resolve(res);
-    }
-
-    function rejectPromiseACB(err) {
-      reject(err);
-    }
-
-    return fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", options)
-      .then(treatHTTPResponseACB)
-      .then(resolvePromiseACB)
-      .catch(rejectPromiseACB);
-  });
+  return APICall(endpoints.popular);
 }
 
 // LATEST
 export function getLatestDrinks() {
-  function throwError() {
-    throw new Error("Something went nuts! Try again!");
-  }
-  function treatHTTPResponseACB(resp) {
-    return resp.status === 200 ? resp.json() : throwError();
-  }
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
-    },
-  };
-
-  return new Promise(async function createPromiseACB(resolve, reject) {
-    function resolvePromiseACB(res) {
-      resolve(res);
-    }
-
-    function rejectPromiseACB(err) {
-      reject(err);
-    }
-
-    return fetch("https://the-cocktail-db.p.rapidapi.com/latest.php", options)
-      .then(treatHTTPResponseACB)
-      .then(resolvePromiseACB)
-      .catch(rejectPromiseACB);
-  });
+  return APICall(endpoints.latest);
 }
