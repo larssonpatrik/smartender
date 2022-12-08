4dfa939a3836dbea8e11ad754cb540ee9362673a
Patrik Larsson <patrik@dictodesigns.com>
2022-12-08 16:58:56 +0100

    working on dropdown

--- src/views/DropDownView.js
+++ src/views/DropDownView.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { TirtiaryButton } from "../components/Buttons";
+import DropDown from "../components/DropDown";
 import Spacer from "../components/Spacer";
+import { listCategories, listGlasses, listIngredients } from "../DrinkSource";
+import promiseNoData from "../promiseNoData";
+import resolvePromise from "../resolvePromise";
 
 export default function DropDownView(props) {
+  const [ingredientPromiseState, setIngredientPromiseState] = React.useState(
+    {}
+  );
+  const [typePromiseState, setTypePromiseState] = React.useState({});
+  const [glassPromiseState, setGlassPromiseState] = React.useState({});
+  const [, reRender] = React.useState({});
+
+  function notifyACB() {
+    reRender({});
+  }
+
   const [ingredientActive, setIngredientActive] = React.useState(false);
   const [typeActive, setTypeActive] = React.useState(false);
   const [glassActive, setGlassActive] = React.useState(false);
@@ -27,14 +42,25 @@ export default function DropDownView(props) {
 
   function renderCurrentActive() {
     if (ingredientActive) {
-      return "Ingredients";
+      return (
+        promiseNoData(ingredientPromiseState) || (
+          <>
+            {console.log("PROMISESTATE", ingredientPromiseState.data.drinks[0])}
+            <DropDown data={ingredientPromiseState.data.drinks} />
+          </>
+        )
+      );
     } else if (typeActive) {
       return "type";
     } else if (glassActive) {
       return "glass";
     }
   }
-
+  React.useEffect(() => {
+    resolvePromise(listIngredients(), ingredientPromiseState, notifyACB);
+    resolvePromise(listCategories(), typePromiseState, notifyACB);
+    resolvePromise(listGlasses(), glassPromiseState, notifyACB);
+  }, []);
   return (
     <div>
       <div
