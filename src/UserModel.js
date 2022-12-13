701e9d243b0ff090ddb7355e9a09a975e82cd72c
Patrik Larsson <patrik@dictodesigns.com>
2022-12-13 11:07:39 +0100

    updated userModel

--- src/UserModel.js
+++ src/UserModel.js
@@ -1,6 +1,68 @@
-class UserModel {
-  constructor(favourites = [], logged_in) {
-    this.favouritedrinks = favourites;
-    this.logged_in = false;
+export default class UserModel {
+  constructor(favoriteDrinks = []) {
+    this.observers = [];
+    this.favoriteDrinks = favoriteDrinks;
+  }
+
+  addObserver(callback) {
+    this.observers = [...this.observers, callback];
+  }
+
+  removeObserver(callback) {
+    function callbackFinderCB(cb) {
+      return cb !== callback;
+    }
+    this.observers = this.observers.filter(callbackFinderCB);
+  }
+
+  notifyObservers(payload) {
+    function invokeObserverCB(observer) {
+      try {
+        observer(payload);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    this.observers.forEach(invokeObserverCB);
+  }
+
+  addToFavorites(addedDrinkId) {
+    let duplicateChecker = false;
+
+    function checkSameId(drinkId) {
+      if (addedDrinkId === drinkId) {
+        duplicateChecker = true;
+      }
+    }
+
+    // Checks for duplicates, does not allow!
+    this.favoriteDrinks.forEach(checkSameId);
+
+    if (!duplicateChecker) {
+      this.favoriteDrinks = [...this.favoriteDrinks, addedDrinkId];
+      this.notifyObservers({ addedDrink: addedDrinkId });
+    }
+  }
+
+  removeFromFavorites(removedDrinkId) {
+    let drinkInFavorites = false;
+
+    function checkForDuplicatesCB(drinkId) {
+      if (removedDrinkId === drinkId) {
+        drinkInFavorites = true;
+      }
+    }
+
+    function filterByIdCB(drinkId) {
+      return removedDrinkId !== drinkId;
+    }
+
+    // Checks for duplicates, does not allow!
+    this.favoriteDrinks.forEach(checkForDuplicatesCB);
+
+    if (drinkInFavorites) {
+      this.favoriteDrinks = this.favoriteDrinks.filter(filterByIdCB);
+      this.notifyObservers({ removedDrink: removedDrinkId });
+    }
   }
 }
