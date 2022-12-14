3f89514486e1200a9ab2c32c637b555b06a658b2
Patrik Larsson <patrik@dictodesigns.com>
2022-12-14 10:23:21 +0100

    added comments to Checkbox

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,12 +4,15 @@ import { BiCheckbox, BiCheckboxChecked } from "react-icons/bi";
 import "../components/css/Checkbox.css";
 
 export default function Checkbox(props) {
+  // Components state that updates the UI depending on option is checked or not
   const [checked, setChecked] = React.useState(false);
 
+  // Event that bubbles up to presenter
   function userSelectsACB() {
     props.action(props.label);
   }
 
+  // Conditional rendering depending on component state
   return checked ? (
     <div className="options" onClick={() => setChecked(!checked)}>
       <METAText style={{ color: "white" }}>{props.label}</METAText>
