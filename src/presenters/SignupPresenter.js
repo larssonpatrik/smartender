e94f1c714dd389c5f9bf0dff8fab5b0f705f8fe3
gretajonsson <gretaj@kth.se>
2022-12-16 13:41:08 +0100

    functionality moved to presenter, Co-Aurthor: Samuel Ribaric,  Patrik Larsson

--- src/presenters/SignupPresenter.js
+++ src/presenters/SignupPresenter.js
@@ -1,9 +1,47 @@
-import Signup from "../views/SignupView";
+import React, { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "../firebaseConfig";
+import "../components/css/Signup.css";
+import "../components/css/Button.css";
+import SignupView from "../views/SignupView";
+const app = initializeApp(firebaseConfig);
 
 export default function SignupPresenter(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function onInputEmailChangeACB(event) {
+    setEmail(event.target.value);
+  }
+
+  function onInputPasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function createUserInFirebaseACB(e) {
+    e.preventDefault();
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  }
+
   return (
     <div>
-      <Signup />
+      <SignupView
+        inputEmail={onInputEmailChangeACB}
+        inputPasssword={onInputPasswordChange}
+        newUser={createUserInFirebaseACB}
+        userPassword={password}
+        userEmail={email}
+      />
     </div>
   );
 }
--- src/views/SignupView.js
+++ src/views/SignupView.js
@@ -1,55 +1,22 @@
-import React, { useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { initializeApp } from "firebase/app";
-import firebaseConfig from "../firebaseConfig";
-import "../components/css/Signup.css";
-const app = initializeApp(firebaseConfig);
+import React from "react";
 
-export default function Signup(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function onInputEmailChangeACB(event) {
-    setEmail(event.target.value);
-  }
-
-  function onInputPasswordChange(event) {
-    setPassword(event.target.value);
-  }
-
-  function createUserInFirebaseACB(e) {
-    e.preventDefault();
-    const auth = getAuth();
-    console.log(email, password);
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-      });
-  }
-
+export default function SignupView(props) {
   return (
     <form>
       <div className="Signupform">
         <input
           className="SignupInput"
-          id="email"
-          type="text"
-          value={email}
-          onChange={onInputEmailChangeACB}
+          value={props.userEmail}
+          onChange={props.inputEmail}
           placeholder="Email"
         ></input>
         <input
           className="SignupInput"
-          value={password}
-          onChange={onInputPasswordChange}
+          value={props.userPassword}
+          onChange={props.inputPasssword}
           placeholder="Password"
         ></input>
-        <button className="SignupButton" onClick={createUserInFirebaseACB}>
+        <button className="PrimaryButton" onClick={props.newUser}>
           SIGN UP
         </button>
       </div>
